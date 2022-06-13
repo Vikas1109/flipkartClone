04ed4f22805d8cc8f07137a0e8b42a574e57ecaf
Vikas Chauhan <vikaschauhan1109@gmail.com>
2022-06-13 12:43:53 +0530

    add route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import NavBar from "./components/common/NavBar";
-import { BrowserRouter, Route, Switch, HashRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Allroutes from "./routes/Allroutes";
 
 function App() {
   return (
     <div>
-      <HashRouter>
+      <BrowserRouter>
         <Allroutes />
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
